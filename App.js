e4d6ef15055ed0868f9bedcb8a8042bcbb3f50d6
Tugay <tasimtugay@gmail.com>
2024-10-21 23:37:08 +0300

    Camera & Saving Pictures

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useRef, useEffect} from 'react';
 
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image, Alert } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 import CameraButton from './frontend/components/camera_button'
 
 export default function App() {
 
   const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+  const [mediaLibraryPermision, requestMediaLibraryPermission] = MediaLibrary.usePermissions();
   const [cameraProps, setCameraProps] = useState({
     zoom: 0,
     facing : 'front',
@@ -17,20 +19,40 @@ export default function App() {
     enableTorch: false
   })
   const [image, setImage] = useState(null);
+  const [previousImage, setPreviousImage] = useState(null);
+
   const cameraRef = useRef(null);
 
-  if(!cameraPermission){
+  // Load the last saved image after permision change
+  useEffect(() => {
+    if (cameraPermission && cameraPermission.granted && mediaLibraryPermision &&
+    mediaLibraryPermision.status === 'granted') {
+        getLastSavedImage();
+    }
+  },  [cameraPermission, mediaLibraryPermision])
+
+  if(!cameraPermission || !mediaLibraryPermision){
     //permission are still loading
-    return <View />
+    return (
+      <View style = {styles.container}>
+        <Text>Error</Text>
+      </View>
+    );
   }
 
-  if (!cameraPermission.granted){
+  if (!cameraPermission.granted || mediaLibraryPermision.status !== 'granted'){
     //permissions are not granted yet
     return (
-      <View>
+      <View style = {styles.permissionContainer}>
         <Text>We need your permission to continue</Text>
-        <TouchableOpacity>
-          <Text>Grant Permission</Text>
+        <TouchableOpacity 
+          style = {styles.permissionButton}
+          onPress={() => {
+            requestCameraPermission();
+            requestMediaLibraryPermission();
+          }}
+        >
+          <Text style = {styles.permissionButtonText}>Grant Permission</Text>
         </TouchableOpacity>
       </View>
     );
@@ -56,19 +78,53 @@ export default function App() {
     }
   }
 
-  const savePicture = () => {
-    console.log('a')
+  // Save taken pictures
+  const savePicture = async() => {
+    if (image) {
+      try{
+        const asset = await MediaLibrary.createAssetAsync(image);
+        const assetInfo = await MediaLibrary.getAssetInfoAsync(asset.id);
+        Alert.alert('Photo saved!', image);
+        setImage(null);
+        getLastSavedImage();
+      }catch (err) {
+        console.log('Error while saving the picture : ', err);
+      }
+    }
   }
+
+  //Get previous taken picture from 'DCIM' album created by expo
+  const getLastSavedImage = async() => {
+    if(mediaLibraryPermision && mediaLibraryPermision.status === 'granted') {
+      const dcimAlbum = await MediaLibrary.getAlbumAsync('DCIM');
+
+      if (dcimAlbum) {
+        const {assets} = await MediaLibrary.getAssetsAsync({
+          album : dcimAlbum,
+          sortBy: [(MediaLibrary.SortBy.creationTime, MediaLibrary.SortBy.default)],
+          mediaType: MediaLibrary.MediaType.photo,
+          first: 1
+        });
+
+        if (assets.length > 0) {
+          const assetInfo = await MediaLibrary.getAssetInfoAsync(assets[0].id);
+          setPreviousImage(assetInfo.localUri || assetInfo.uri);
+        }
+        else{
+          setPreviousImage(null);
+        }
+      }
+      else{
+        setPreviousImage(null);
+      }
+    }
+  } 
 
   return (
     <View style={styles.container}>
       {!image ? (
         <>
           <View style = {styles.topControlsContainer}>
-            <CameraButton 
-              icon = 'flip-camera-ios'
-              onPress={() => toggleProperty('facing', 'front', 'back')}
-            />
             <CameraButton 
               icon = {cameraProps.flash === 'on' ? 'flash-on' : 'flash-off'}
               onPress={() => toggleProperty('flash', 'on', 'off')}
@@ -92,18 +148,35 @@ export default function App() {
             enableTorch = {cameraProps.enableTorch}
             ref={cameraRef}
           />
+
           <View style = {styles.bottomControlsContainer}>
+            <TouchableOpacity onPress={() => previousImage && setImage(previousImage)}>
+
+              <Image 
+                source={{uri:previousImage}}
+                style={styles.previousImage}
+              />
+            </TouchableOpacity>
             <CameraButton
               icon = 'camera'
               size = {60}
               style={{height: 60}}
               onPress = {takePicture}
             />
+            <CameraButton 
+              icon = 'flip-camera-ios'
+              size={60}
+              style={{height: 60}}
+              onPress={() => toggleProperty('facing', 'front', 'back')}
+            />
           </View>
         </>
       ) : (
         <>
-          <Image source = {{uri:image}} style = {styles.camera}/>
+          <Image 
+            source = {{uri:image}} 
+            style = {styles.camera}/>
+
           <View style = {styles.bottomControlsContainer}>
             <CameraButton 
               icon = 'flip-camera-android'
@@ -128,6 +201,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  permissionContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
   topControlsContainer: {
     height: 100,
     backgroundColor: 'black',
@@ -147,5 +226,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     flexDirection: 'row',
     justifyContent: 'space-around',
+  },
+
+  permissionButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 10,
+    borderRadius: 2
+  },
+  permissionButtonText: {
+    color : 'white',
+    fontSize: 16
+  },
+  previousImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 50
   }
 });
